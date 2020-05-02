e103a109f3fc5b69e22deacf697e710628d286a0
Kate Weaver <kweave23@jhu.edu>
2020-05-02 02:48:30 -0400

    work on legend and overall plot look

--- README.md
+++ README.md
@@ -2,9 +2,28 @@
 This repository contains the code for analysis of health-rating *C. elegans* drug screening experiments. All code is based off the analysis used in [this paper](https://journals.plos.org/plosone/article?id=10.1371/journal.pone.0179376)<br />
 
 ### Required inputs
-Use the `--toAssess` argument to pass the experiment input files. (`--toAssess kw9_export.txt kw11_export.txt kw12_export.txt` will pass 3 input files). <br /><br />
+Use the `--toAssess` argument to pass the experiment/data input files. (`--toAssess kw9_export.txt kw11_export.txt kw12_export.txt` will pass 3 input files). <br /><br />
 
-The input files should be tab delimited and formatted like this example beginning 37 lines of `kw9_export.txt` where each line specifies a well at a given concentration on a given day and the number of worms that were scored as 0, 1, 2, or 3 for that well/day/concentration as well as the total number of worms in the well, and a boolean argument for the final column in only the first day's set of lines.
+If performing IT50/LT50 analysis, `--representative` is a **required** argument. (ex: `--representative 3`). See [this part of the documentation](https://github.com/ellis22b/worm-analysis#it50lt50-specific-notes-and-required-argument) for specifics and explanation about this argument.
+
+The following set of inputs will be used to annotate plots from the data passed to `--toAssess`
+- Use `--strain` to specify the strain of *C. elegans* was treated in the assays
+- Use `--lifestage` to specify which stage of *C. elegans* was treated in the assays
+- Use `--drug` to specify which drug was used for treatment in the assays (concentration is assumed to be ug/mL)
+- Use `--expNames` to specify the names of the experiments to be assessed (ex: `--expNames kw9 kw11 kw12`). Order of the names should match the order of the files.
+- Use `--concUnits` to specify the units of the concentrations. The actual values will be inferred from the input files. `--concUnits 0` would use ug/mL in labeling plots and tables. `--concUnits 7` would use mg/kg in labeling plots and tables
+  - 0: ug/mL
+  - 1: uM
+  - 2: M
+  - 3: mM
+  - 4: nM
+  - 5: ng/mL
+  - 6: mg/mL
+  - 7: mg/kg
+  - 8: mg/g
+Another important note about the data input files is that **all inputs (at this time) in a single run should be experiments with the same strain, life stage, drug, number of days, and concentrations**. For example, in the `--toAssess` list above, kw9, kw11, and kw12 were N2 L4 Albendazole 7-day experiments with concentrations 0, 0.1, 1, 10, 100, 1000 ug/mL. (Later additions to the program can explore ways to plot drug screening more efficiently)<br />< br />
+
+Concerning **format for the input files**: These input files should be **tab delimited** and formatted like this example beginning 37 lines of `kw9_export.txt` where each line specifies a well at a given concentration on a given day and the number of worms that were scored as 0, 1, 2, or 3 for that well/day/concentration as well as the total number of worms in the well, and a boolean argument for the final column in only the first day's set of lines.
 ```
 Well	Concentration	Day	num0_in_well	num1_in_well	num2_in_well	num3_in_well	numTotal_in_well	numTotal_equal
 1	0	1	0	0	0	8	8	TRUE
@@ -45,8 +64,6 @@ Well	Concentration	Day	num0_in_well	num1_in_well	num2_in_well	num3_in_well	numTo
 18	1000	2	0	0	5	1	6
 ```
 
-If performing IT50/LT50 analysis, `--representative` is a **required** argument.
-
 ### Note on default analysis
 Note that the default is to run and plot all analysis including the following. In order to not assess any of the following, set the specified argument to false ( `--plotLine3 False` will cause the program to skip plotting motility index score)
 - motility index score by concentration over the length of the experiment (`--plotLine3`)
@@ -55,6 +72,11 @@ Note that the default is to run and plot all analysis including the following. I
 - LC50 (`--plotLC50`)
 - IT50 (`--plotIT50`)
 - LT50 (`--plotLT50`)
+- report the number of worms (`--reportNum`)
+
+### Line Plots of daily motility and mortality response by concentration specific notes and arguments
+In the [2017 paper](https://journals.plos.org/plosone/article?id=10.1371/journal.pone.0179376), these graphs combined the scores for each individual well in each experiment for a given concentration to find the average score. These are the plots that will be produced by default by this program if `--plotLine3` and/or `--plotLine1` are true.<br /><br />
+In addition to this default behavior, the argument `--include_single_exp_plots` can be used to produce plots for each individual experiment. `--include_single_exp_plots` is false by default. When true (in combination with `--plotLine3` and/or `--plotLine1`), the program will also plot each experiment by itself in addition to the default plots.
 
 ### IC50/LC50 specific notes and arguments
 If running IC50 and/or LC50, the default is to consider day 4 data. The user can change this using the `--C_day` argument. `--C_day 5` would analyze day 5. <br /><br />
@@ -64,6 +86,8 @@ For IC50 and LC50 experiments, the process is based off of the [prism equation](
 If running, IT50 and/or LT50, the user must define the representative experiment to run, using the `--representative` argument. Specifically, provide the number corresponding to the index (1 based index) of the representative experiment in the `--toAssess` argument. If `--toAssess kw9_export.txt kw11_export.txt kw12_export.txt` and the user wants kw12 to be the representative experiment, then use `--representative 3`. For kw11 as representative use `--representative 2`. If a representative experiment is not defined by the user, the script will exit with a message after performing all other analysis. <br /><br />
 
 ### General notes on the program
+The argument `--reportNum` is true by default and will report the total number of worms assessed by concentration (corresponding to [Table 1 of the 2017 paper](https://journals.plos.org/plosone/article?id=10.1371/journal.pone.0179376)). These total numbers will be reported in a text file named `table1_equiv_numTreated_[drug]_[stage]_[strain].txt`, where drug, stage, and strain are dependent on the inputs. Specifically if `table1_equiv_numTreated_ALB_L4_N2.txt` would be the file from input arguments `--drug ALB --lifestage L4 --strain N2`. The logfile will also record the name of the file written too.
+
 The number of days and concentrations of a drug in an experiment are inferred from the input data. Any inconsistencies in a well number's concentration across experiments raises an error. <br /><br />
 
 A logfile stores the user defined settings to run the analysis, warnings, and info pertaining to derived values and parameters from curve-fitting. The logfile name can be specified using the `--logfile` argument. Otherwise the default is set to reflect the date and time that the analysis was run.<br /><br />
--- ic50_test.py
+++ ic50_test.py
@@ -11,11 +11,19 @@ from scipy.optimize import curve_fit
 import logging
 import pandas as pd
 import datetime
+import matplotlib.pyplot as plt
+import sys
+from matplotlib import rc
+
+# activate latex text rendering
+rc('text', usetex=True)
 
 def main():
     parser = generate_parser()
     args = parser.parse_args()
-    analysis_instance = WormAnalysis(args.toAssess, args.logfile)
+    analysis_instance = WormAnalysis(args.toAssess, args.logfile, args.drug, args.strain, args.stage, args.concUnits, args.reportNum)
+    if args.plotLine3 or args.plotLine1:
+        analysis_instance.driveLinePlots(args.plotLine3, args.plotLine1, args.isep, args.expNames)
     #if args.plotIC50 or args.plotLC50:
     #    analysis_instance.driveIC(args.plotIC50, args.plotLC50, args.C_day, args.x0_val)
 
@@ -23,8 +31,14 @@ def main():
 def generate_parser():
     parser = ap.ArgumentParser(description='C elegans analysis')
     parser.add_argument('--toAssess', action='store', dest='toAssess', nargs='+', type=str, required=True, help='the exported files to assess')
+    parser.add_argument('--expNames', action='store', dest='expNames', nargs='+', type=str, required=True, help='the names of the experiments passed in toAssess (same order); will be used to annotate plots and tables')
+    parser.add_argument('--strain', action='store', dest='strain', type=str, required=True, help='the strain which was treated in the assays (ex. N2 or Hawaii); will be used to annotate plots and tables')
+    parser.add_argument('--lifestage', action='store', dest='stage', type=str, required=True, help='the lifestage which was treated in the assays (ex. L1 or L4); will be used to annotate plots and tables')
+    parser.add_argument('--drug', action='store', dest='drug', type=str, required=True, help='the drug used in treatment in the assays (ex. ALB, PYR, IVM, or NTZ, etc); will be used to annotate plots and tables; for now this only accepts a single value')
+    parser.add_argument('--concUnits', action='store', dest='concUnits', type=int, required=True, help='use to specify the units of the concentration. 0 is ug/mL, 1 is uM, 2 is M, 3 is mM, 4 is nM, 5 is ng/mL, 6 is mg/mL, 7 is mg/kg, 8 is mg/g')
     parser.add_argument('--plotLine3', action='store', dest='plotLine3', type=bool, default=True, help='whether to plot the daily motility response by concentration')
     parser.add_argument('--plotLine1', action='store', dest='plotLine1', type=bool, default=True, help='whether to plot the daily lethality response by concentration')
+    parser.add_argument('--include_single_exp_plots', action='store', dest='isep', type=bool, default=False, help='when plotting the daily motility or lethalty response by concentration, whether to plot single experiments as well as the average')
     parser.add_argument('--plotIC50', action='store', dest='plotIC50', type=bool, default=True, help='whether to plot the IC50 (3-2-1-0 scoring)')
     parser.add_argument('--plotLC50', action='store', dest='plotLC50', type=bool, default=True, help='whether to plot the LC50 (1-0 scoring)')
     parser.add_argument('--C_day', action='store', dest='C_day', type=int, default=4, help='the day (index from 1) to assess for inhibitory and/or lethal concentration')
@@ -32,15 +46,39 @@ def generate_parser():
     parser.add_argument('--plotIT50', action='store', dest='plotIT50', type=bool, default=True, help='whether to plot the IT50 (3-2-1-0 scoring)')
     parser.add_argument('--plotLT50', action='store', dest='plotLT50', type=bool, default=True, help='whether to plot the LT50 (3-2-1-0 scoring)')
     parser.add_argument('--representative', action='store', dest='rep', type=int, default=0, help='which number (specifying order/location) of the input files (1, 2, or 3, etc) that is the representative to be used for I/LT50')
+    parser.add_argument('--reportNum', action='store', dest='reportNum', type=bool, default=True, help='report the total number of worms in each concentration (summed across all input experiments), corresponding to Table 1 of the 2017 paper')
     parser.add_argument('--logfile', action='store', dest='logfile', type=str, default='worm_analysis_{}.txt'.format(datetime.datetime.now()), help='the logfile to store information and any warning messages')
+
     return parser
 
 class WormAnalysis():
-    def __init__(self, toAssess, logfile):
+    def __init__(self, toAssess, logfile, drug, strain, stage, concUnits, reportNum):
+        self.drug = drug
+        self.strain = strain
+        self.stage = stage
+        self.concUnits = concUnits
+
+        self.concUnits_dict = { 0: "\u03bcg/mL",
+                                1: "\u03bcM",
+                                2: "M",
+                                3: "mM",
+                                4: "nM",
+                                5: "ng/mL",
+                                6: "mg/mL",
+                                7: "mg/kg",
+                                8: "mg/g" }
+        self.conc_colors_lo_to_hi = ['black', 'darkorange', 'darkgoldenrod', 'purple', 'limegreen', 'blue']
+        self.conc_markers_lo_to_hi = ['s','o', '^', 'v', 'd', 'o']
+        self.conc_marker_outline_lo_to_hi = ['black', 'darkorange', 'darkgoldenrod', 'purple', 'limegreen', 'black']
+        #use the following _ u'{}'.format(self.concUnits) _ to add the concentration as a label if args.concUnits is 0 or 1
         logging.basicConfig(filename=logfile, level=logging.INFO)
+        logging.info("command used to run analysis:\n {}".format(' '.join(sys.argv)))
         self.load_data(toAssess)
+        if reportNum:
+            self.find_numTotal()
         self.find_motility_index_score()
         self.find_mortality_score()
+
 
     def load_data(self, toAssess):
         self.num_experiments = len(toAssess)
@@ -89,15 +127,30 @@ class WormAnalysis():
             if num_trues != np.sum(df.loc[:,'numTotal_equal']):
                 logging.warning('wells in {} do not have equal numbers of worms in a given well across the length of the experiment'.format(file))
 
+    def find_numTotal(self):
+        total_nums = np.sum(self.scores3_by_conc[:,:,0,:].reshape(-1, 4*self.num_experiments), axis=1)
+        filename = 'table1_equiv_numTreated_{}_{}_{}.txt'.format(self.drug, self.stage, self.strain)
+        toWriteTo = open(filename, 'w+')
+        toWriteTo.write('Number of nematodes treated\n{} on {} {} _C. elegans_\n'.format(self.drug, self.stage, self.strain))
+        for i in range(self.uniq_conc.shape[0]):
+            toWriteTo.write('{} {}:\t{}\n'.format(self.uniq_conc[i], self.concUnits_dict[self.concUnits], int(total_nums[i])))
+        toWriteTo.close()
+        logging.info("Wrote total number of nematodes treated by concentration to the file {}".format(filename))
+
     def find_motility_index_score(self):
         '''setting motility index score
         Day 0 is automatically a score of 3 for all wells
         weight the number of worms in well of score 0 by 0, the number of worms in well of score 1 by 1, the number of worms in well of score 2 by 2, and the number of worms in well of score 3 by 3
         Sum these weighted numbers and then divide by the total number of worms in well '''
-        self.motility_index_scores = np.full((self.num_concentrations, self.num_days+1, self.num_experiments), 3.0, dtype=np.float64) #day 0 will be set to 3 automatically, will set days1 onwards at the end
+        self.motility_index_scores_by_conc = np.full((self.num_concentrations, self.num_days+1, self.num_experiments), 3.0, dtype=np.float64) #day 0 will be set to 3 automatically, will set days1 onwards at the end
         adjusted_sum = np.sum(self.scores3_by_conc * np.array([0, 1, 2, 3]).reshape((1,-1,1,1)), axis=1) #weight number of worms by the score and sum: 0*num0_in_well + 1*num1_in_well + 2*num2_in_well + 3*num3_in_well
         divided = adjusted_sum/np.sum(self.scores3_by_conc, axis=1) #divide the sum above (adjusted_sum) by the number of worms total in the well (the denominator)
-        self.motility_index_scores[:, 1:, :] = divided #setting days1 onwards
+        self.motility_index_scores_by_conc[:, 1:, :] = divided #setting days1 onwards
+
+        self.motility_index_scores_by_well = np.full((self.num_concentrations*3, self.num_days+1, self.num_experiments), 3.0, dtype=np.float64)
+        adjusted_sum_by_well = np.sum(self.scores3_by_well * np.array([0, 1, 2, 3]).reshape((1,-1,1,1)), axis=1)
+        divided_by_well = adjusted_sum_by_well/np.sum(self.scores3_by_well, axis=1)
+        self.motility_index_scores_by_well[:, 1:, :] = divided_by_well
 
     def find_mortality_score(self):
         '''setting percent alive
@@ -105,10 +158,59 @@ class WormAnalysis():
         weight the nubmer of worms in well of score 0 by 0 and weight the number of worms in well of score 1 by 1, the number of worms in well by score 2 by 1, and the nubmer of worms in well of score 3 by 1
         Sum these weighted number to effectively sum the number of worms in the well that are alive
         then divide by the total number of worms in the well and multiply 100 to convert to percentage'''
-        self.mortality_scores = np.full((self.num_concentrations, self.num_days+1, self.num_experiments), 100.0, dtype=np.float64) #day 0 will be set to 100 automatically, will set days1 onwards at the end
+        self.mortality_scores_by_conc = np.full((self.num_concentrations, self.num_days+1, self.num_experiments), 100.0, dtype=np.float64) #day 0 will be set to 100 automatically, will set days1 onwards at the end
         adjusted_sum = np.sum(self.scores3_by_conc * np.array([0, 1, 1 ,1]).reshape((1, -1, 1, 1)), axis=1) #weight number of worms by the score and sum: 0*num0_in_well + 1*num1_in_well + 1*num2_in_well + 1*num3_in_well where the score 1 denotes alive
         divided_to_percent = adjusted_sum/np.sum(self.scores3_by_conc, axis=1)*100 #divide the sum above (adjusted_sum) by the number of worms total in the well (the denominator), change to percentage
-        self.mortality_scores[:, 1:, :] = divided_to_percent
+        self.mortality_scores_by_conc[:, 1:, :] = divided_to_percent
+
+        self.mortality_scores_by_well = np.full((self.num_concentrations*3, self.num_days+1, self.num_experiments), 100.0, dtype=np.float64)
+        adjusted_sum_by_well = np.sum(self.scores3_by_well * np.array([0, 1, 1, 1]).reshape((1, -1, 1, 1)), axis=1)
+        divided_to_percent_by_well = adjusted_sum_by_well/np.sum(self.scores3_by_well, axis=1)*100
+        self.mortality_scores_by_well[:, 1:, :] = divided_to_percent_by_well
+
+    def plotLineCurves(self, toPlot, figname, title, ylabel, ymin, ymax):
+        '''right now this doesn't include specifying molarity of the highest concentration'''
+        fig, ax = plt.subplots()
+        fig.suptitle(title)
+        days_arr = np.arange(self.num_days + 1)
+        for i in range(toPlot.shape[0])[::-1]:
+            if self.concUnits == 0 or self.concUnits==1:
+                label = u"{} {}".format(self.uniq_conc[i], self.concUnits_dict[self.concUnits])
+            else:
+                label = "{} {}".format(self.uniq_conc[i], self.concUnits_dict[self.concUnits])
+            ax.plot(days_arr, toPlot[i], c=self.conc_colors_lo_to_hi[i], marker=self.conc_markers_lo_to_hi[i], markeredgecolor=self.conc_marker_outline_lo_to_hi[i], label=label)
+        ax.set_xlabel('Days')
+        ax.set_ylabel(ylabel)
+        ax.set_ylim(ymin, ymax)
+        #ax.legend()
+        fig.savefig(figname)
+        plt.close(fig)
+        logging.info('Plotted the figure {}'.format(figname))
+
+    def driveLinePlots(self, plotLine3, plotLine1, isep, expNames):
+        '''Do we want to include the functionality to compute and plot stdev?'''
+        if plotLine3:
+            if isep:
+                for i, exp in enumerate(expNames):
+                    self.plotLineCurves(self.motility_index_scores_by_conc[:, :, i], '{}_isep_motility.png'.format(exp), r"%s %s on %s %s \textit{C. elegans}" %(exp, self.drug, self.stage, self.strain), "Motility Index Score", 0, 3)
+            reshaped_mid_by_well = self.motility_index_scores_by_well.reshape((self.num_concentrations, 3, self.num_days+1, self.num_experiments))
+            motility_index_across_exp = np.zeros((self.num_concentrations, 3*self.num_experiments, self.num_days+1), dtype=np.float64)
+            for j in range(self.num_experiments):
+                motility_index_across_exp[:,j*3:(j*3)+3 ,:] = reshaped_mid_by_well[:,:,:,j]
+            motility_avg_across_exp = np.mean(motility_index_across_exp, axis=1)
+            self.plotLineCurves(motility_avg_across_exp, 'average_motility_{}_{}_{}.png'.format(self.drug, self.stage, self.strain), r"%s on %s %s \textit{C. elegans}" %(self.drug, self.stage, self.strain), "Motility Index Score", 0, 3)
+
+        if plotLine1:
+            if isep:
+                for i, exp in enumerate(expNames):
+                    self.plotLineCurves(self.mortality_scores_by_conc[:, :, i], '{}_isep_mortality.png'.format(exp), r"%s %s on %s %s \textit{C. elegans}" %(exp, self.drug, self.stage, self.strain), "Percent Alive", 0, 100)
+            reshaped_mort_by_well = self.mortality_scores_by_well.reshape((self.num_concentrations, 3, self.num_days+1, self.num_experiments))
+            mortality_across_exp = np.zeros((self.num_concentrations, 3*self.num_experiments, self.num_days+1), dtype=np.float64)
+            for j in range(self.num_experiments):
+                mortality_across_exp[:,j*3:(j*3)+3, :] = reshaped_mort_by_well[:,:,:,j]
+            mortality_avg_across_exp = np.mean(mortality_across_exp, axis=1)
+            self.plotLineCurves(mortality_avg_across_exp, 'average_mortality_{}_{}_{}.png'.format(self.drug, self.stage, self.strain), r"%s on %s %s \textit{C. elegans}" %(self.drug, self.stage, self.strain), "Percent Alive", 0, 100)
+
 
     def transform_X(self, X):
         '''set [0] to x0_val'''
